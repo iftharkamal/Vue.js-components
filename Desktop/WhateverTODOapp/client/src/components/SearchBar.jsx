7c782fc14bd59d7ac3f7ba5ccc80086ecb3e2c8f
iftharkamal <iftharkamal@gmail.com>
2025-02-17 11:53:26 +0530

    title and description added

---
+++ Desktop/WhateverTODOapp/client/src/components/SearchBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const SearchBar = () => {
+  return (
+    <>
+
+    <input type="text" placeholder='Search here...' className='border border-gray-300 py-3 px-6 w-[60%] rounded-full outline-none text-gray-600 ' />
+      
+    </>
+  )
+}
+
+export default SearchBar
